28f5e542902dc88e8a0bea1233cf2598b0ac7494
Turrirri <Picure01@yahoo.com>
2026-01-17 17:41:17 -0400

    actualizados plugins y añadido el ejecutar programas con la tecla 'F5'

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local runner_group = vim.api.nvim_create_augroup("CodeRunner", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "lua", "javascript" },
+  callback = function()
+    local ft = vim.bo.filetype
+    local cmd = ""
+
+    if ft == "python" then
+      cmd = "python3 " .. vim.fn.expand("%")
+    elseif ft == "lua" then
+      cmd = "lua " .. vim.fn.expand("%")
+    elseif ft == "javascript" then
+      cmd = "node " .. vim.fn.expand("%")
+    end
+
+    -- Configuración del mapeo
+    vim.keymap.set("n", "<F5>", function()
+      vim.cmd("write") -- Guarda el archivo
+      -- Abre un split de 10 líneas abajo y ejecuta el comando
+      vim.cmd("split | term " .. cmd)
+      -- Cambia a modo insertar automáticamente para ver el output
+      vim.cmd("startinsert")
+    end, { buffer = true, desc = "Ejecutar archivo actual" })
+  end,
+  group = runner_group,
+})
