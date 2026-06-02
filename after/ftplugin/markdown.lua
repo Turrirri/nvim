c9df00447ec322f16bd7f03bcd141019886c07f2
Turrirri <Picure01@yahoo.com>
2026-06-02 07:23:38 -0400

    Añadida corrector ortográfico
    se creó un archivo ftplugin apar que realisaze la corrección ortográfiac
    en archivos .md y .txt

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+-- Habilitar el corrector ortográfico
+vim.opt_local.spell = true
+
+-- Configurar los idiomas (inglés y español)
+vim.opt_local.spelllang = { "en", "es" }
--- nvim/lua/plugins/debuggers.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-  }
-}
