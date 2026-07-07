2539696498347db32e0c8e2b055f5817c3e222f5
Turrirri <Picure01@yahoo.com>
2026-07-07 07:52:24 -0400

    Reinstalado nvim
    Otra oportunidad para neovim 0.12

--- init.lua
+++ init.lua
@@ -2,3 +2,23 @@
 require("config.lazy")
 require("ui.line_numbers")
 -- vim.cmd.colorscheme("tokyonight-moon")
+-- Función para transparentar el fondo
+local function transparent_background()
+  local hl_groups = {
+    "Normal", "NormalFloat", "SignColumn", "NormalNC",
+    "TelescopeBorder", "TelescopeNormal", "NvimTreeNormal",
+    "NeoTreeNormal", "NeoTreeNormalNC"
+  }
+  for _, group in ipairs(hl_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+  end
+end
+
+-- Ejecutar la función al iniciar y cada vez que cambie el colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = transparent_background,
+})
+
+-- Llamada inicial por si acaso
+transparent_background()
--- lua/plugins/nvim-surround-wk.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "gregorias/nvim-surround-wk",
-    config = true,
-  },
-}
